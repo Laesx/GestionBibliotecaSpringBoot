15ea06755784d11a392bbc069404e9ba49eb1a6e
Laesx <losguardianes15@gmail.com>
2024-02-05 11:02:33 +0100

    Initial commit

---
+++ src/test/java/xanketes/gestionbibliotecaspringboot/GestionBibliotecaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package xanketes.gestionbibliotecaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBibliotecaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
